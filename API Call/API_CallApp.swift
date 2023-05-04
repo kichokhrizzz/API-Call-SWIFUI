c0b09ff72474435200c0147fa180d12f22328fde
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-05-03 20:19:32 -0600

    Initial Commit

---
+++ API Call/API_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallApp.swift
+//  API Call
+//
+//  Created by Jhosel Badillo Cortes on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
